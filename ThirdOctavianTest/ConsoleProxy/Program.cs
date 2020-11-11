dd510c86b43e8a22332b1cbbc479767c33321d53
Сергий Ермилов <Corvusincvr@gmail.com>
2020-11-11 13:57:23 +0300

    add client

---
+++ ThirdOctavianTest/ConsoleProxy/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ServiceReference1;
+
+namespace ConsoleProxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListOfUserServiceClient client = new ListOfUserServiceClient();
+            var resultDep = client.GetDepartamentAsync(25);
+            Console.WriteLine(resultDep.Result);
+        }
+    }
+}
