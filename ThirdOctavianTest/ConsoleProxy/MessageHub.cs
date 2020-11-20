ef8ac81d766ab9b9788720b18ae63db2c2d11179
Сергий Ермилов <Corvusincvr@gmail.com>
2020-11-20 11:07:25 +0300

    Added SignalR Hub (Server) in .Net Core Console

---
+++ ThirdOctavianTest/ConsoleProxy/MessageHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using System.Text;
+
+namespace ConsoleProxy
+{
+    public class MessageHub : Hub
+    {
+        public async Task SendMessage(string userName, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", userName, message);
+        }
+    }
+}
--- ThirdOctavianTest/ConsoleProxy/Program.cs
+++ ThirdOctavianTest/ConsoleProxy/Program.cs
@@ -1,5 +1,12 @@
 ﻿using System;
 using ServiceReference1;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ConsoleProxy
 {
@@ -7,9 +14,37 @@ namespace ConsoleProxy
     {
         static void Main(string[] args)
         {
+            CreateDefaultBuilder(args).Build().Run();
+
             ListOfUserServiceClient client = new ListOfUserServiceClient();
             var resultDep = client.GetUserDetailsAsync(GetCommandDB.GetAllUser).Result;
             
         }
+
+        private static IWebHostBuilder CreateDefaultBuilder(string[] args) =>
+           WebHost.CreateDefaultBuilder(args)
+              .UseStartup<Startup>();
+    }
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+        }
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<MessageHub>("/MessageHub");
+            });
+        }
+
     }
 }
