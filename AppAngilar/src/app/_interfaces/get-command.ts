5597b6557bcf9bcaf1d771636e6f24d61da5747c
Сергий Ермилов <Corvusincvr@gmail.com>
2020-11-22 15:59:49 +0300

    returned from DB

---
+++ AppAngilar/src/app/_interfaces/get-command.ts
@@ -0,0 +1,6 @@
+export enum GetCommandDB
+{
+  GetAllUser=0,
+  GetAllRole=1,
+  GetAllDept=2,
+}
--- AppAngilar/src/app/_service/signal-r.service.ts
+++ AppAngilar/src/app/_service/signal-r.service.ts
@@ -1,28 +1,32 @@
 import { Injectable } from '@angular/core';
 import * as signalR from '@aspnet/signalr';
 import { User } from '../_interfaces/user';
+import { GetCommandDB } from '../_interfaces/get-command';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignalRService {
   private hubConnection: signalR.HubConnection;
+  public com: GetCommandDB;
   public us: User;
-  public data: string
+  public data: User[];
   public startConnection = () => {
     console.log('Work');
     this.hubConnection = new signalR.HubConnectionBuilder()
       .withUrl('https://localhost:5001/MessageHub')
       .build();
-    this.us = { Login: 'Privet', Id: 24 };
+    this.com = 0;
+    //this.us = { Login: 'Privet', Id: 24 };
     this.hubConnection
       .start()
       .then(() => {
-        this.hubConnection.invoke('Send', this.us);
+        this.hubConnection.invoke('GetRequest', this.com);
       });
+
   }
   public addReceive = () => {
-    this.hubConnection.on('Receive', (data) => {
+    this.hubConnection.on('SetArray', (data) => {
       this.data = data;
       console.log(data);
     });
