175c2ec363176c01ef8c7b724c65edfb1c687bf0
Сергий Ермилов <Corvusincvr@gmail.com>
2020-11-27 12:25:53 +0300

    Create all method WCF

--- ThirdOctavianTest/ConsoleProxy/MessageHub.cs
+++ ThirdOctavianTest/ConsoleProxy/MessageHub.cs
@@ -20,27 +20,33 @@ namespace ConsoleProxy
             ListOfUserServiceClient client = new ListOfUserServiceClient();
             var resultList = client.GetRolesAsync().Result;
             await Clients.All.SendAsync("addArrayRoles", resultList);
-        } 
+        }
         public async Task GetDepts()
         {
             ListOfUserServiceClient client = new ListOfUserServiceClient();
-
             var resultList = client.GetDeptsAsync().Result;
             await Clients.All.SendAsync("addArrayDepts", resultList);
         }
-
-        public async Task SetRequest(User userDetails, ProcedureDB SetCommand)
+        public async Task SetUser(User uDetails, ProcedureDB SetCommand)
         {
             ListOfUserServiceClient client = new ListOfUserServiceClient();
-
-            await client.SetUserDetailsAsync(userDetails, SetCommand);
-            Console.WriteLine("SetMethod");
-            await Clients.All.SendAsync("linkMethod", "RETUNED");
+            await client.SetUserDetailsAsync(uDetails, SetCommand);
+            Console.WriteLine("StartUserSetMethod");
+            await Clients.All.SendAsync("linkMethod", "UserWCFMethodActivate");
         }
-        public async Task Send(string message)
+        public async Task SetRole(Role rDetails, ProcedureDB SetCommand)
         {
-            Console.WriteLine(message);
-            await Clients.All.SendAsync("Receive", message);
+            ListOfUserServiceClient client = new ListOfUserServiceClient();
+            await client.SetRoleDetailsAsync(rDetails, SetCommand);
+            Console.WriteLine("StartRoleSetMethod");
+            await Clients.All.SendAsync("linkMethod", "RoleWCFMethodActivate");
+        }
+        public async Task SetDept(Dept dDetails, ProcedureDB SetCommand)
+        {
+            ListOfUserServiceClient client = new ListOfUserServiceClient();
+            await client.SetDeptDetailsAsync(dDetails, SetCommand);
+            Console.WriteLine("StartDeptSetMethod");
+            await Clients.All.SendAsync("linkMethod", "DeptWCFMethodActivate");
         }
     }
 }
